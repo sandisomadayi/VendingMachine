4b037f3dbe33dea6659b4f0e2195fe9bb2a7b3c4
Sandiso Madayi <sandisomadayi239@gmail.com>
2019-10-09 11:26:06 +0200

    testing MaxiVendingMachine

--- tests/vending_machine/MaxiVendingMachineTest.java
+++ tests/vending_machine/MaxiVendingMachineTest.java
@@ -1,6 +1,10 @@
 package vending_machine;
 
 import org.junit.jupiter.api.Test;
+import product_exceptions.ChocolatesAllGone;
+import product_exceptions.ProductNotFoundException;
+import product_exceptions.SaltyCracksAllEatenException;
+import product_exceptions.SoftDrinksOutOfStockException;
 import products.Chocolate;
 import products.Product;
 import products.SaltySnack;
@@ -13,26 +17,49 @@ public class MaxiVendingMachineTest {
     @Test
     void shouldDisplayTotalStock() {
         Product fanta = new SoftDrink("fanta", "500ml bottle", 6.99);
-        Product simba = new SaltySnack("lays", "500g bag", 16.99);
+        Product lays = new SaltySnack("lays", "500g bag", 16.99);
         Product aero = new Chocolate("aero", "200g slab", 23.99);
 
         MaxiVendingMachine maxiVendingMachine = new MaxiVendingMachine();
-        maxiVendingMachine.addStock(fanta, 4);
-        maxiVendingMachine.addStock(simba, 4);
-        maxiVendingMachine.addStock(aero, 4);
-
-        assertEquals(12, maxiVendingMachine.getStockLevel());
+        try {
+            maxiVendingMachine.addStock(fanta, 4);
+            maxiVendingMachine.addStock(lays, 4);
+            maxiVendingMachine.addStock(aero, 4);
+            maxiVendingMachine.buy(lays);
+        } catch (SoftDrinksOutOfStockException so) {
+            so.printStackTrace();
+        } catch (SaltyCracksAllEatenException sa) {
+            sa.printStackTrace();
+        } catch (ChocolatesAllGone ch) {
+            ch.printStackTrace();
+        } catch (ProductNotFoundException pr) {
+            pr.printStackTrace();
+        }
+        assertEquals(11, maxiVendingMachine.getStockLevel());
     }
     @Test
     void shouldDisplaySoftDrinkStock() {
-        Product fanta = new SoftDrink("fanta", "500ml bottle", 6.99);
-        Product simba = new SaltySnack("lays", "500g bag", 16.99);
-        Product aero = new Chocolate("aero", "200g slab", 23.99);
 
-        MaxiVendingMachine maxiVendingMachine = new MaxiVendingMachine();
-        maxiVendingMachine.addStock(fanta, 4);
-        maxiVendingMachine.addStock(simba, 4);
-        maxiVendingMachine.addStock(aero, 4);
+            Product fanta = new SoftDrink("fanta", "500ml bottle", 6.99);
+            Product lays = new SaltySnack("lays", "500g bag", 16.99);
+            Product aero = new Chocolate("aero", "200g slab", 23.99);
+
+            MaxiVendingMachine maxiVendingMachine = new MaxiVendingMachine();
+
+        try {
+            maxiVendingMachine.addStock(fanta, 4);
+            maxiVendingMachine.addStock(lays, 4);
+            maxiVendingMachine.addStock(aero, 4);
+            maxiVendingMachine.buy(aero);
+        } catch (SoftDrinksOutOfStockException so) {
+            so.printStackTrace();
+        } catch (SaltyCracksAllEatenException sa) {
+            sa.printStackTrace();
+        } catch (ChocolatesAllGone ch) {
+            ch.printStackTrace();
+        } catch (ProductNotFoundException pr) {
+            pr.printStackTrace();
+        }
 
         assertEquals("fanta remaining: 4", maxiVendingMachine.getStockLevel(fanta));
     }
@@ -43,23 +70,50 @@ public class MaxiVendingMachineTest {
         Product aero = new Chocolate("aero", "200g slab", 23.99);
 
         MaxiVendingMachine maxiVendingMachine = new MaxiVendingMachine();
-        maxiVendingMachine.addStock(fanta, 4);
-        maxiVendingMachine.addStock(lays, 4);
-        maxiVendingMachine.addStock(aero, 4);
+        try {
+            maxiVendingMachine.addStock(fanta, 4);
+            maxiVendingMachine.addStock(lays, 4);
+            maxiVendingMachine.addStock(aero, 4);
+            maxiVendingMachine.buy(aero);
+            maxiVendingMachine.buy(aero);
+            maxiVendingMachine.buy(aero);
+        } catch (SoftDrinksOutOfStockException so) {
+            so.printStackTrace();
+        } catch (SaltyCracksAllEatenException sa) {
+            sa.printStackTrace();
+        } catch (ChocolatesAllGone ch) {
+            ch.printStackTrace();
+        } catch (ProductNotFoundException pr) {
+            pr.printStackTrace();
+        }
 
         assertEquals("lays remaining: 4", maxiVendingMachine.getStockLevel(lays));
     }
     @Test
     void shouldDisplayChocolateStock() {
         Product fanta = new SoftDrink("fanta", "500ml bottle", 6.99);
-        Product simba = new SaltySnack("lays", "500g bag", 16.99);
+        Product lays = new SaltySnack("lays", "500g bag", 16.99);
         Product aero = new Chocolate("aero", "200g slab", 23.99);
 
         MaxiVendingMachine maxiVendingMachine = new MaxiVendingMachine();
-        maxiVendingMachine.addStock(fanta, 4);
-        maxiVendingMachine.addStock(simba, 4);
-        maxiVendingMachine.addStock(aero, 4);
 
-        assertEquals("aero remaining: 4", maxiVendingMachine.getStockLevel(aero));
+        try {
+            maxiVendingMachine.addStock(fanta, 4);
+            maxiVendingMachine.addStock(lays, 4);
+            maxiVendingMachine.addStock(aero, 4);
+            maxiVendingMachine.buy(aero);
+            maxiVendingMachine.buy(aero);
+            maxiVendingMachine.buy(aero);
+        } catch (SoftDrinksOutOfStockException so) {
+            so.printStackTrace();
+        } catch (SaltyCracksAllEatenException sa) {
+            sa.printStackTrace();
+        } catch (ChocolatesAllGone ch) {
+            ch.printStackTrace();
+        } catch (ProductNotFoundException pr) {
+            pr.printStackTrace();
+        }
+
+        assertEquals("aero remaining: 1", maxiVendingMachine.getStockLevel(aero));
     }
 }
